93035a4df44df4c5570b6cd1f255e2d6754becd9
makiroggers <makiroggers@gmail.com>
2017-09-19 19:08:54 -0700

    Added user queries files.

---
+++ Django/forms_project/apps/users/models.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.db import models
+from django.utils import timezone
+# from datetime import datetime
+# from time import strftime
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
---
+++ Django/forms_project/apps/users/views.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render, HttpResponse, redirect
+from .models import User
+
+
+def index(request):
+    context = {
+        'all_users': User.objects.all(),
+        'last_user': User.objects.order_by('-created_at').reverse()[:1],
+        'first_user': User.objects.first(),
+        'all_users_first_name_desc': User.objects.order_by('first_name'),
+        # following commands update field
+        'get_record_3': User.objects.get(id=3),
+        'update_record_3': "get_record_3.last_name='Kinne'.save()",
+        'save_record_3': get_record_3.save(),
+        'delete_record_4': User.objects.get(id=4).delete(),
+    }
+    return render(request, 'users/index.html', context)
+
+"""
+VALIDATION IN SHELL COMMANDS:
+for user in User.objects.all():
+    ...:     if len(user.first_name) < 5:
+    ...:         print user.first_name
+    ...:     else:
+    ...:         print "All good"
+
+"""
